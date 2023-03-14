21db35cc3b45a7a8004340a5b6ca4c517548038c
Arjun Singh Tomar <109807311+arjundevlop@users.noreply.github.com>
2023-03-14 13:01:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const form = document.querySelector('#myForm');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const nameInput = document.querySelector('#name');
+  const ageInput = document.querySelector('#age');
+  
+  // check if inputs are empty
+  if (nameInput.value === '' || ageInput.value === '') {
+    alert('Please fill out all fields.');
+    return;
+  }
+  
+  const age = parseInt(ageInput.value);
+  const name = nameInput.value;
+	const promise = new Promise((resolve,reject)=>{
+		if(age>18){
+			setTimeout(()=>{
+				resolve();
+			},4000);
+		}
+		else{
+			reject();
+		}
+	});
+
+	promise.then(()=>{
+		alert(`Welcome, ${name}. You can vote.`);
+	}).catch(()=>{
+		alert(`Oh sorry ${name}. You aren't old enough.`);
+	})
+	form.reset();
+});
